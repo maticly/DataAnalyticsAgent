5d4812e702c3b0f65d6c58749095636d13ce4c67
Mati <mateuszcly@gmail.com>
2025-11-09 23:10:39 -0500

    Create BaseTool abstract class
    Added an abstract base class for tools with required properties and methods.

--- scr/tools/base.py
+++ scr/tools/base.py
@@ -1,1 +1,28 @@
+class BaseTool(ABC):
+    """Abstract base class for all tools."""
 
+    @property
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def description(self) -> str:
+        pass
+
+    @property
+    @abstractmethod
+    def parameters(self) -> Dict[str, Any]:
+        pass
+
+    @abstractmethod
+    def execute(self, **kwargs) -> Dict[str, Any]:
+        pass
+
+    def to_dict(self) -> Dict[str, Any]:
+        return {
+            "name": self.name,
+            "description": self.description,
+            "parameters": self.parameters
+        }
